d71ca332312925591414aada8193e4b1e2abe73f
Rasyid <Rasyidridla298@gmail.com>
2022-03-30 19:48:36 +0700

    Initial Commit

---
+++ PuzzleKu/PuzzleKuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleKuApp.swift
+//  PuzzleKu
+//
+//  Created by Rasyid Ridla on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleKuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
